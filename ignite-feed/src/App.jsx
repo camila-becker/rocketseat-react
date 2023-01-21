f967c3c4f542e37b09e024776d0168fc4137dbee
Camila Becker <camila.becker01@gmail.com>
2023-01-21 10:54:28 -0300

    Ignite Feed: criado componente de sidebar

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -1,8 +1,18 @@
 import { Header } from "./components/Header/Header";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
