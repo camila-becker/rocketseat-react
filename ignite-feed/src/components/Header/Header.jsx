c98e223a634043eff44a5b3935fe45c3a82432e5
Camila Becker <camila.becker01@gmail.com>
2023-01-21 10:09:34 -0300

    Ignite Feed: Modificado o header

--- ignite-feed/src/components/Header/Header.jsx
+++ ignite-feed/src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ export function Header() {
   return (
     <header className={styles.header}>
       <img src={igniteLogo} alt="Logotipo do Ignite" />
+      <h1>Ignite Feed</h1>
     </header>
   );
 }
