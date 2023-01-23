9eb5d314ededd7f21cee7cfaf1a4e933d3f73dc0
Camila Becker <camila.becker01@gmail.com>
2023-01-23 18:58:16 -0300

    Ignite Feed: criado componente Avatar

--- ignite-feed/src/components/Post/Post.jsx
+++ ignite-feed/src/components/Post/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "../Avatar/Avatar";
 import { Comment } from "../Comment/Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/camila-becker.png"
-          />
+          <Avatar src="https://github.com/camila-becker.png" />
           <div className={styles.authorInfo}>
             <strong>Camila Becker</strong>
             <span>Desenvolvedora FullStack</span>
--- ignite-feed/src/components/Sidebar/Sidebar.jsx
+++ ignite-feed/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "../Avatar/Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -10,10 +11,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/camila-becker.png"
-        />
+        <Avatar src="https://github.com/camila-becker.png" />
         <strong>Camila Becker</strong>
         <span>Desenvolvedora FullStack</span>
       </div>
