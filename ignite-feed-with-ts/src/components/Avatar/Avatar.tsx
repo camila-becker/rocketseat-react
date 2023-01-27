775a75ef347e85a1fbb2dfcd3edb751d4f7ad200
Camila Becker <camila.becker01@gmail.com>
2023-01-27 16:34:03 -0300

    Ignite Feed With TS: Ajuste no componente de Avatar

--- ignite-feed-with-ts/src/components/Avatar/Avatar.tsx
+++ ignite-feed-with-ts/src/components/Avatar/Avatar.tsx
@@ -5,12 +5,11 @@ interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
   hasBorder?: boolean;
 }
 
-export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...props}
     />
   );
 }
