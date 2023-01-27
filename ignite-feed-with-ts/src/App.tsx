3d05689bffc34c8aafbd65e62b17beb20f4b3f34
Camila Becker <camila.becker01@gmail.com>
2023-01-27 16:23:00 -0300

    Ignite Feed With TS: Adicionado projeto Ignite Feed desenvolvido com TypeScript

---
+++ ignite-feed-with-ts/src/App.tsx
@@ -0,0 +1,70 @@
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Post } from "./components/Post/Post";
+
+import "./global.css";
+import styles from "./App.module.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/camila-becker.png",
+      name: "Camila Becker",
+      role: "Desenvolvedora FullStack @CWI",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "camila.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-01-24 14:40:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/tiago-oliveira.png",
+      name: "Tiago Oliveira",
+      role: "Desenvolvedor FrontEnd @CWI",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "camila.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-01-22 15:00:00"),
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
