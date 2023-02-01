41eab0a024bf4050c2de492bec13af43cf578e96
Camila Becker <camila.becker01@gmail.com>
2023-02-01 11:44:12 -0300

    Desafio TodoList: Projeto Finalizado

--- desafios/todo-list/src/components/Task/Task.tsx
+++ desafios/todo-list/src/components/Task/Task.tsx
@@ -1,16 +1,41 @@
 import { Trash } from "phosphor-react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import styles from "./Task.module.css";
 
-export function Task() {
+interface Task {
+  id: string;
+  title: string;
+  isComplete: boolean;
+}
+
+interface TaskProps {
+  task: Task;
+  onCompletedTask: (completed: boolean) => void;
+  onDeleteTask: (task: string) => void;
+}
+
+export function Task({ task, onCompletedTask, onDeleteTask }: TaskProps) {
+  const [completedTask, setCompletedTask] = useState(false);
+
+  function handleCompletedTask(event: ChangeEvent<HTMLInputElement>) {
+    task.isComplete = event.target.checked;
+    setCompletedTask(task.isComplete);
+    return onCompletedTask(task.isComplete);
+  }
+
+  function handleDeleteTask(event: FormEvent) {
+    event.preventDefault();
+    return onDeleteTask(task.id);
+  }
+
   return (
     <>
       <form className={styles.task}>
-        <input type="checkbox" />
-        <label>
-          Integer urna interdum massa libero auctor neque turpis turpis semper.
-          Duis vel sed fames integer.
+        <input type="checkbox" onChange={handleCompletedTask} />
+        <label className={completedTask ? styles.taskCompleted : ""}>
+          {task.title}
         </label>
-        <button>
+        <button onClick={handleDeleteTask}>
           <Trash size={20} />
         </button>
       </form>
--- desafios/todo-list/src/components/Todo/Todo.tsx
+++ desafios/todo-list/src/components/Todo/Todo.tsx
@@ -1,12 +1,62 @@
-import { PlusCircle } from "phosphor-react";
+import { ClipboardText, PlusCircle } from "phosphor-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { Task } from "../Task/Task";
 import styles from "./Todo.module.css";
 
+interface Task {
+  id: string;
+  title: string;
+  isComplete: boolean;
+}
+
 export function Todo() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState("");
+  const [countCompleteTasks, setCountCompleteTasks] = useState(0);
+
+  const hasTask = tasks.length > 0;
+
+  function handleNewTask(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function handleCreateTask(event: FormEvent) {
+    event.preventDefault();
+    setTasks([
+      ...tasks,
+      {
+        id: uuidv4(),
+        title: newTask,
+        isComplete: false,
+      },
+    ]);
+    setNewTask("");
+  }
+
+  function completeTask(completed: boolean) {
+    completed
+      ? setCountCompleteTasks((state) => state + 1)
+      : setCountCompleteTasks((state) => state - 1);
+  }
+
+  function deleteTask(taskToDelete: string) {
+    const tasksWithoutDeletedOne = tasks.filter((task) => {
+      return taskToDelete !== task.id;
+    });
+    setTasks(tasksWithoutDeletedOne);
+  }
+
   return (
     <>
-      <form className={styles.createTodo}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
+      <form className={styles.createTodo} onSubmit={handleCreateTask}>
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          onChange={handleNewTask}
+          value={newTask}
+          required
+        />
         <button type="submit">
           Criar
           <PlusCircle size={18} />
@@ -16,15 +66,36 @@ export function Todo() {
         <div className={styles.info}>
           <strong className={styles.infoCreated}>
             Tarefas criadas
-            <span>0</span>
+            <span>{tasks.length}</span>
           </strong>
           <strong className={styles.infoDone}>
             Concluídas
-            <span>0</span>
+            <span>
+              {hasTask
+                ? `${countCompleteTasks} de ${tasks.length}`
+                : tasks.length}
+            </span>
           </strong>
         </div>
-        <div className={styles.todo}>
-          <Task />
+        <div className={!hasTask ? styles.todo : styles.todoWithoutBorder}>
+          {hasTask ? (
+            tasks.map((task) => {
+              return (
+                <Task
+                  key={task.id}
+                  task={task}
+                  onCompletedTask={completeTask}
+                  onDeleteTask={deleteTask}
+                />
+              );
+            })
+          ) : (
+            <div className={styles.emptyTask}>
+              <ClipboardText size={56} className={styles.clipboard} />
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          )}
         </div>
       </div>
     </>
