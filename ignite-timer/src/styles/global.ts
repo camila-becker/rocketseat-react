cebd1e252a08efd2b8b5a01e8aa0be1ed6eb5dee
Camila Becker <camila.becker01@gmail.com>
2023-02-20 16:22:17 -0300

    Ignite Timer: criado o componente de Home

--- ignite-timer/src/styles/global.ts
+++ ignite-timer/src/styles/global.ts
@@ -10,12 +10,17 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${props => props.theme["green-500"]};
+    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
+  }
+
+  html {
+    height: 100vh;
   }
 
   body {
-    background-color: ${props => props.theme["gray-900"]};
-    color: ${props => props.theme["gray-300"]};
+    background-color: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
